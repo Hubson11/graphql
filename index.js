fcce66773f4a8aba72ee5292fea9a06847e66824
Hubert <h.worwa@livespace.io>
2021-02-08 23:33:25 +0100

    Add some data to graph server

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = `
-    type Query {
-        greetings: String
-    }
-`
-const server = new ApolloServer({ typeDefs });
+const typeDefs = gql`
+  type Query {
+    greetings: String
+    interestingUrls: [String]
+    first_name: String
+    email: String
+    pets: [String]
+  }
+`;
 
-server.listen({ port: 4000 }).then((result) => console.log(result.url, 'result'))
+const data = {
+  greetings: "Hello world!",
+  interestingUrls: ["https://kursreacta.pl", "https://64bites.com"],
+  first_name: "John",
+  email: "john@example.com",
+  pets: ["Mittens", "Doggo", "Birb"],
+};
+
+const server = new ApolloServer({ typeDefs, rootValue: data });
+
+server
+  .listen({ port: 4000 })
+  .then((result) => console.log(result.url, "result"));
